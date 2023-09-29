641cadc214c445b241a6a49d8ed5679e0a8274bc
Ethan <ethanhoughting@gmail.com>
2023-09-29 14:17:07 -0400

    Rest of files for lab

---
+++ src/BuddyInfo.java
@@ -0,0 +1,16 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private int phoneNumber;
+    public BuddyInfo(String name, String address, int phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo Bob = new BuddyInfo("Bob", "Carleton", 647);
+        System.out.println("Hello " + Bob.name + "!");
+    }
+}
